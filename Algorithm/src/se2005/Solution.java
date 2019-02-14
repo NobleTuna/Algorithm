c8ee52c0b569dceaec2b813aa31a88357f4ba366
lyy7661 <lyy7661@naver.com>
2019-02-15 08:49:45 +0900

    se2005 start

---
+++ Algorithm/src/se2005/Solution.java
@@ -0,0 +1,16 @@
+package se2005;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = T; tc <= T; tc++) {
+
+		}
+	}
+
+}
