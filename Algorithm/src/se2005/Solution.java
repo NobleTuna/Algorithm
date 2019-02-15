ac25b2ce4cd8ccf7ba54c7b31285dc0bd57a65e1
lyy7661 <lyy7661@naver.com>
2019-02-15 13:36:08 +0900

    se2005 뽀갬

--- Algorithm/src/se2005/Solution.java
+++ Algorithm/src/se2005/Solution.java
@@ -8,8 +8,25 @@ public class Solution {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		for (int tc = T; tc <= T; tc++) {
-
+		for (int tc = 1; tc <= T; tc++) {
+			int[] before = new int[10];
+			before[0]=1;
+			before[1]=1;
+			int N = Integer.parseInt(br.readLine())+1;
+			System.out.print("#"+tc);
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<i; j++) {
+					if(j ==0 ||j==i-1) {
+						
+						System.out.print(1+" ");
+					}else {
+						int k= before[j-1]+before[j];
+						System.out.print(k+" ");
+						before[j]=k;
+					}
+					
+				}System.out.println();
+			}
 		}
 	}
 
