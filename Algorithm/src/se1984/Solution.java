73c1aa769dac3de4eb90a1c99335f16b70ef3921
lyy7661 <lyy7661@naver.com>
2019-02-15 16:36:26 +0900

    1984 뽀갬

---
+++ Algorithm/src/se1984/Solution.java
@@ -0,0 +1,37 @@
+package se1984;
+
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			int arr[] = new int[10];
+			for (int i = 0; i < 10; i++) {
+				arr[i] = sc.nextInt();
+			}
+			int min = Integer.MAX_VALUE;
+			int max = Integer.MIN_VALUE;
+			for (int i = 0; i < 10; i++) {
+				if (arr[i] < min)
+					min = arr[i];
+				if (arr[i] > max)
+					max = arr[i];
+			}
+			double sum = 0;
+			for (int i = 0; i < 10; i++) {
+				if (arr[i] == min || arr[i] == max) {
+					continue;
+				}
+				sum += arr[i];
+			}
+			double result = sum/8;
+			
+			System.out.println("#"+tc+" "+Math.round(result));
+
+		}
+
+	}
+
+}
