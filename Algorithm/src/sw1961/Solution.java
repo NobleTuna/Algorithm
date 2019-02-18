e79697a307c88c3710e66b02d1ec7caf5cf008b6
NobleTuna <lyy7661@naver.com>
2019-02-18 23:25:14 +0900

    sw1961 뽀갬

--- Algorithm/src/sw1961/Solution.java
+++ Algorithm/src/sw1961/Solution.java
@@ -1,4 +1,4 @@
-package sw1961_;
+package sw1961;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -7,12 +7,11 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		System.setIn(new FileInputStream("input.txt"));
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		
-		for(int tc = 0; tc<=T; tc++) {
+		for(int tc = 1; tc<=T; tc++) {
 			
 			int N = Integer.parseInt(br.readLine());
 			int map[][] = new int[N][N];
@@ -27,14 +26,34 @@ public class Solution {
 			int [][]res2 = new int[N][N];
 			int [][]res3 = new int[N][N];
 			
+			
 			for(int i=0; i<N; i++) {
 				for(int j=0; j<N; j++) {
-					res1[i][j] = map[j][2-i]; 
+					res1[j][i] = map[N-1-j][i];  
 				}
 			}
 			for(int i=0; i<N; i++) {
 				for(int j=0; j<N; j++) {
-					System.out.print(res1[i][j]+" ");
+					res2[i][j] = map[N-1-j][N-1-i];  
+				}
+			}
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N; j++) {
+					res3[j][i] = map[j][N-1-i];  
+				}
+			}
+			System.out.println("#"+tc);
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N; j++) {
+					System.out.print(res1[j][i]);
+				}
+				System.out.print(" ");
+				for(int j=0; j<N; j++) {
+					System.out.print(res2[j][i]);
+				}
+				System.out.print(" ");
+				for(int j=0; j<N; j++) {
+					System.out.print(res3[j][i]);
 				}System.out.println();
 			}
 		}
