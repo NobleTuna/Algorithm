2efab0d55828f00accaea4cd12d22d13f7e4d0fc
lyy7661 <lyy7661@naver.com>
2019-02-19 14:35:12 +0900

    개판됨

---
+++ Algorithm/src/sw1954/Solution.java
@@ -0,0 +1,100 @@
+package sw1954;
+
+import java.util.Scanner;
+
+public class Solution {
+	static int F;
+	static int N;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			N = sc.nextInt();
+			F = N * N;
+			int map[][] = new int[N][N];
+			boolean visited[][] = new boolean[N][N];
+			map[0][0] = 1;
+			visited[0][0] = true;
+			right(map, visited, 0, 0, 2);
+			System.out.println("#"+tc);
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N; j++) {
+					System.out.print(map[i][j]+" ");
+				}System.out.println();
+			}
+		}
+	}
+
+	public static void right(int map[][], boolean[][] visited, int y, int x, int cnt) {
+		if (cnt == F + 1) {
+			return;
+		}
+//		System.out.println("right");
+		for (int i = 1; i <= N; i++) {
+			int nX = x + i;
+			if (nX >= N || visited[y][nX]) {
+//				System.out.println("go down");
+				down(map, visited, y, nX - 1, cnt);
+				break;
+			}
+//			System.out.println(y+" "+nX);
+			visited[y][nX] = true;
+			map[y][nX] = cnt++;
+		}
+
+	}
+
+	public static void down(int map[][], boolean[][] visited, int y, int x, int cnt) {
+		if (cnt == F+1 ) {
+			return;
+		}
+//		System.out.println("down");
+//		System.out.println(nY+" "+x);
+		for (int i = 1; i <= N; i++) {
+			int nY = y + i;
+			
+			if (nY >= N || visited[nY][x]) {
+				left(map, visited, nY - 1, x, cnt);
+				break;
+			}
+			
+			visited[nY][x] = true;
+			map[nY][x] = cnt++;
+		}
+
+	}
+
+	public static void left(int map[][], boolean[][] visited, int y, int x, int cnt) {
+		if (cnt == F +1) {
+			return;
+		}
+		for (int i = 1; i <= N; i++) {
+			int nX = x - i;
+			if (nX < 0 || visited[y][nX]) {
+				up(map, visited, y, nX + 1, cnt);
+				break;
+			}
+			visited[y][nX] = true;
+			map[y][nX] = cnt++;
+		}
+
+	}
+
+	public static void up(int map[][], boolean[][] visited, int y, int x, int cnt) {
+		if (cnt == F + 1) {
+			return;
+		}
+		for (int i = 1; i <= N; i++) {
+			int nY = x - i;
+			if (nY < 0 || visited[nY][x]) {
+				right(map, visited, nY + 1, x, cnt);
+				break;
+			}
+			visited[nY][x] = true;
+			map[nY][x] = cnt++;
+		}
+
+	}
+
+}
---
+++ Algorithm/src/sw3124/Solution.java
@@ -0,0 +1,82 @@
+package sw3124;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static class info implements Comparable<info> {
+		int cost;
+		int next;
+
+		public info(int cost, int next) {
+			this.cost = cost;
+			this.next = next;
+		}
+
+		public int compareTo(info o) {
+			if (this.cost > o.cost)
+				return 1;
+			else if (this.cost < o.cost)
+				return -1;
+			return 0;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			PriorityQueue<info> q = new PriorityQueue<info>();
+			String input = br.readLine();
+			StringTokenizer st = new StringTokenizer(input);
+			int V = Integer.parseInt(st.nextToken());
+			int E = Integer.parseInt(st.nextToken());
+			PriorityQueue<info>[] arr = new PriorityQueue[V + 1];
+			for (int i = 0; i < V + 1; i++) {
+				arr[i] = new PriorityQueue<info>();
+			}
+			for (int i = 0; i < E; i++) {
+				input = br.readLine();
+				st = new StringTokenizer(input);
+				int r = Integer.parseInt(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+				int cost = Integer.parseInt(st.nextToken());
+				arr[r].add(new info(cost, c));
+			}
+			long result = 0;
+
+			boolean[] visited = new boolean[V + 1];
+			List<Integer> list = new ArrayList<Integer>();
+			visited[1] = true;
+			list.add(1);
+			for (int i = 0; i < V - 1; i++) {
+				int min = Integer.MAX_VALUE;
+				int K = 0;
+				for (int k : list) {
+					if (!arr[k].isEmpty()) {
+						while (!arr[k].isEmpty() && visited[arr[k].peek().next]) {
+							arr[k].poll();
+						}
+						if (!arr[k].isEmpty() &&!visited[arr[k].peek().next] && min > arr[k].peek().cost) {
+							K = k;
+						}
+					}
+				}
+				visited[arr[K].peek().next] = true;
+				list.add(arr[K].peek().next);
+				result += arr[K].peek().cost;
+				arr[K].poll();
+
+			}
+			System.out.println("#" + tc + " " + result);
+
+		}
+	}
+}
