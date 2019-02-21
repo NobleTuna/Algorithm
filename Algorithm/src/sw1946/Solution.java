dc3ba338fcd1227317749097ab234e732629d81b
NobleTuna <lyy7661@naver.com>
2019-02-21 22:45:47 +0900

    sw1946 뽀갬

--- Algorithm/src/sw1946/Solution.java
+++ Algorithm/src/sw1946/Solution.java
@@ -13,19 +13,26 @@ public class Solution {
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
 			int N = Integer.parseInt(br.readLine());
-			List<String> list = new ArrayList<String>();
-			
+
 			// sb형 리스트
-			for (int i = 0; i < N; i++) {
+			StringBuilder sb = new StringBuilder();
+			for (int n = 0; n < N; n++) {
 				String input = br.readLine();
 				StringTokenizer st = new StringTokenizer(input);
 				char alpa = st.nextToken().charAt(0);
-				int k = Integer.parseInt(br.readLine());
-				
-				
-
+				int k = Integer.parseInt(st.nextToken());
+				for (int i = 0; i < k; i++) {
+					sb.append(alpa);
+				}
 			}
-
+			System.out.println("#"+tc);
+			
+			for(int i=1; i<sb.length()+1; i++) {
+				System.out.print(sb.charAt(i-1));
+				if(i%10==0)
+					System.out.println();
+			}
+			System.out.println();
 		}
 
 	}
