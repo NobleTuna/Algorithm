46a548a8a52f7f6cb13a872852bb78956668b36d
NobleTuna <lyy7661@naver.com>
2019-02-23 13:51:18 +0900

    base64 자바 라이브러리

---
+++ Algorithm/src/sw1928/Solution.java
@@ -0,0 +1,21 @@
+package sw1928;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Base64;
+
+public class Solution {
+	public static void main(String[] args) throws  IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			String msg = br.readLine();
+			String result = new String(Base64.getDecoder().decode(msg));
+			System.out.println("#"+tc+" "+result);
+		}
+	}
+
+}
