1770667d80a64bc263b13b3bd0e007244a89059f
lyy7661 <lyy7661@naver.com>
2019-02-26 10:21:46 +0900

    sw4751

--- Algorithm/src/sw4751_/Solution.java
+++ Algorithm/src/sw4751_/Solution.java
@@ -1,11 +1,60 @@
 package sw4751_;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Solution {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			String input = br.readLine();
+			char[] output = input.toCharArray();
+			
+			///
+			System.out.print("..");
+			for (int i = 0; i < output.length - 1; i++) {
+				System.out.print("#...");
+			}
+			System.out.print("#..");
+			System.out.println();
+			
+			/////
+			
+			System.out.print(".");
+			for(int i=0; i<output.length; i++) {
+				System.out.print("#.#.");
+			}
+			System.out.println();
+			
+			///
+			
+			
+			for(int i=0; i<output.length; i++) {
+				System.out.print("#."+output[i]+".");
+			}System.out.println("#");
+			
+			
+			System.out.print(".");
+			for(int i=0; i<output.length; i++) {
+				System.out.print("#.#.");
+			}
+			System.out.println();
+			
+			System.out.print("..");
+			for (int i = 0; i < output.length - 1; i++) {
+				System.out.print("#...");
+			}
+			System.out.print("#..");
+			System.out.println();
+			
+			/////
+			
+
+		}
+		
 		
 	}
 
