d3c6870bbdf2f61ac71626cb9897d717f36da04a
NobleTuna <lyy7661@naver.com>
2019-03-04 22:35:04 +0900

    d3 40% 돌파

--- Algorithm/src/sw3233/Solution.java
+++ Algorithm/src/sw3233/Solution.java
@@ -1,8 +1,29 @@
 package sw3233;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Solution {
-	public static void main(String[] args) {
-		
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			String input = br.readLine();
+			StringTokenizer st = new StringTokenizer(input);
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+
+			int C = A / B;
+
+			Long result = (long) Math.pow(C, 2);
+
+			System.out.println("#" + tc + " " + result);
+
+		}
 	}
 
 }
