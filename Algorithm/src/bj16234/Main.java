7cc3816f83c52035dec6fce8d3a78f317283f258
lyy7661 <lyy7661@naver.com>
2019-03-06 08:59:37 +0900

    r

---
+++ Algorithm/src/bj16234/Main.java
@@ -0,0 +1,21 @@
+package bj16234;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		StringTokenizer st = new StringTokenizer(input);
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+		
+		
+
+	}
+
+}
