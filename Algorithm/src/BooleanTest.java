edd947ab814a1bd8c082f08e232d10d084b7d600
NobleTuna <lyy7661@naver.com>
2019-07-18 23:29:10 +0900

    boolean chk

---
+++ Algorithm/src/BooleanTest.java
@@ -0,0 +1,15 @@
+
+public class BooleanTest {
+	
+	public static final Boolean FALSE = new Boolean(false);
+	
+	static Boolean myBool = new Boolean(false);
+	
+	static Boolean myBool2 = Boolean.FALSE;
+	
+	public static void main(String[] args) {
+		System.out.println(myBool);
+		System.out.println(myBool2);
+	}
+
+}
