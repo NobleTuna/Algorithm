eaf5f25cc17b56e340681e51a8187565339fc7f9
NobleTuna <lyy7661@naver.com>
2019-07-19 22:55:24 +0900

    sw5953 solving

---
+++ Algorithm/src/sw5953_newNumRuls/Solution.java
@@ -0,0 +1,21 @@
+package sw5953_newNumRuls;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+	static Long N;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			
+			Long N = Long.parseLong(br.readLine());
+			
+			
+		}
+	}
+}
