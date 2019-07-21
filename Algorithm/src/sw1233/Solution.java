d9a06e1d8f8c6e1c6a4370ba81b0fbe17db73d70
NobleTuna <lyy7661@naver.com>
2019-07-21 21:47:39 +0900

    sw1233 푸는중

---
+++ Algorithm/src/sw1233/Solution.java
@@ -0,0 +1,21 @@
+package sw1233;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		for(int tc = 1; tc<=10; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			
+			for(int n=0; n<N; n++) {
+				
+			}
+		}
+				
+	}
+
+}
