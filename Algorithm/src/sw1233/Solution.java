272b48ec924fe67cba96ac8f4ab84e1843957e36
NobleTuna <lyy7661@naver.com>
2019-07-22 23:16:25 +0900

    sw1233 푸는중

--- Algorithm/src/sw1233/Solution.java
+++ Algorithm/src/sw1233/Solution.java
@@ -3,6 +3,7 @@ package sw1233;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -12,7 +13,9 @@ public class Solution {
 			int N = Integer.parseInt(br.readLine());
 			
 			for(int n=0; n<N; n++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
 				
+				// 2의 n승?
 			}
 		}
 				
