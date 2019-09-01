43fba08e52410f2e932eef5e4f8a2d42d43aa206
NobleTuna <lyy7661@naver.com>
2019-09-01 16:57:35 +0900

    kakao FailureRate pass

--- Algorithm/src/kakao_2018_FailureRate/Solution.java
+++ Algorithm/src/kakao_2018_FailureRate/Solution.java
@@ -3,13 +3,24 @@ package kakao_2018_FailureRate;
 import java.util.Arrays;
 
 public class Solution {
-	static class StageAndFailure {
+	static class StageAndFailure implements Comparable<StageAndFailure> {
 		int stageNum;
 		double failure;
 
 		public StageAndFailure(int stageNum, double failure) {
 			this.stageNum = stageNum;
 			this.failure = failure;
+		}
+
+		@Override
+		public int compareTo(StageAndFailure o) {
+			if (o.failure > this.failure)
+				return 1;
+			else if (o.failure < this.failure)
+				return -1;
+
+			return this.stageNum - o.stageNum;
+
 		}
 	}
 
@@ -20,7 +31,6 @@ public class Solution {
 			userOnStage[s]++;
 		}
 
-		System.out.println(Arrays.toString(userOnStage));
 
 		StageAndFailure[] arr = new StageAndFailure[N + 2];
 		for (int idx = 0; idx < arr.length; idx++) {
@@ -29,30 +39,34 @@ public class Solution {
 
 		int beforeUsers = 0;
 		int reachUser = stages.length;
-		System.out.println("reachUser : " + reachUser);
 
 		for (int idx = 1; idx < userOnStage.length; idx++) {
 			int tmp = (reachUser - beforeUsers);
-			double failure = userOnStage[idx] / tmp;
-			System.out.println("tmp : " + tmp);
-			System.out.println("userOnStage : " + userOnStage[idx]);
-			System.out.println("failure : " + failure);
+			double failure = (double) userOnStage[idx] / (double) tmp;
 			beforeUsers += userOnStage[idx];
 			arr[idx] = new StageAndFailure(idx, failure);
 		}
 
-//		for (int i = 0; i < arr.length; i++) {
-//			System.out.println(arr[i].failure);
-//		}
+		Arrays.sort(arr);
+
 		int[] answer = new int[N];
+		int idx = 0;
+		for (StageAndFailure s : arr) {
+			if(s.stageNum == 0 || s.stageNum == N+1)
+				continue;
+			answer[idx++] = s.stageNum;
+			
+		}
+//		System.out.println(Arrays.toString(answer));
 
 		return answer;
 	}
 
-	static int[] stages = { 2, 1, 2, 6, 2, 4, 3, 3 };
+	static int[] stages1 = { 2, 1, 2, 6, 2, 4, 3, 3 };
+	static int[] stages2 = {4,4,4,4,4};
 
 	public static void main(String[] args) {
 		Solution sol = new Solution();
-		sol.solution(5, stages);
+		sol.solution(5, stages1);
 	}
 }
