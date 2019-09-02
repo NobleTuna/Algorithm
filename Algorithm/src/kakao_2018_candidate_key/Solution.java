12dfc1f5ac80e36579cb89fe9a85409e1f0ccfd7
NobleTuna <lyy7661@naver.com>
2019-09-02 20:16:39 +0900

    카카오 후보키

---
+++ Algorithm/src/kakao_2018_candidate_key/Solution.java
@@ -0,0 +1,18 @@
+// https://www.welcomekakao.com/learn/courses/30/lessons/42890
+package kakao_2018_candidate_key;
+
+class Solution {
+	public int solution(String[][] relation) {
+		int answer = 0;
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		String[][] TC1 = { { "100", "ryan", "music", "2" }, { "200", "apeach", "math", "2" },
+				{ "300", "tube", "computer", "3" }, { "400", "con", "computer", "4" }, { "500", "muzi", "music", "3" },
+				{ "600", "apeach", "music", "2" } };
+		
+		Solution sol = new Solution();
+		sol.solution(TC1);
+	}
+}
