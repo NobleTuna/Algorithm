3cc447e78f23c4bf86b105c0cecf7948632bd25b
NobleTuna <lyy7661@naver.com>
2019-10-28 22:54:54 +0900

    test 제네릭, 와일드카드 테스트 내용없음 이슈없음

--- Algorithm/src/ListTest.java
+++ Algorithm/src/ListTest.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 public class ListTest {
 
@@ -25,6 +27,23 @@ public class ListTest {
 		ListClass<ListClass, Integer> list1 = new ListClass<ListTest.ListClass, Integer>(list2, Integer.MAX_VALUE);
 		list2.a = list1;
 		List<Integer> list = new ArrayList<Integer>();
+
+		ArrayList<String> places = new ArrayList<String>(Arrays.asList("Buenos Aires", "Córdoba", "La Plata"));
+
+		List<?> wildCard = new ArrayList<>();
+		List<? extends Object> wc = new ArrayList<Object>();
+		List<? extends Object> wc1 = new ArrayList<String>(Arrays.asList("가", "나", "다"));
+		List<? extends Number> wc2 = new ArrayList<Double>(Arrays.asList(33.44, 241.5));
+		Number e = 3;
+		for (Object k : wc1) {
+			System.out.println(k);
+		}
+		for (Number k : wc2) {
+			System.out.println(k);
+		}
+
+		List<String> sList = new ArrayList<String>();
+		sList.add("kk");
 
 	}
 
