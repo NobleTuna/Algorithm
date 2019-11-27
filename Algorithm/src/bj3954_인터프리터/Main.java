c24b966cf8d60f888c2cd8881b130eef4d35b986
NobleTuna <lyy7661@naver.com>
2019-11-27 17:49:45 +0900

    bj3954 28% 틀렸습니다 원인 알수가 없음

--- Algorithm/src/bj3954_인터프리터/Main.java
+++ Algorithm/src/bj3954_인터프리터/Main.java
@@ -7,10 +7,10 @@ import java.io.StringReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Solution {
+public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		br = new BufferedReader(new StringReader(TC));
+		br = new BufferedReader(new StringReader(TC3));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int T = Integer.parseInt(st.nextToken());
@@ -18,14 +18,15 @@ public class Solution {
 			st = new StringTokenizer(br.readLine());
 			int M = Integer.parseInt(st.nextToken()); // 메모리 배열 크기
 			int C = Integer.parseInt(st.nextToken()); // 프로그램 코드 크기
-			int I = Integer.parseInt(st.nextToken()); // 입력 크기
+			int I = Integer.parseInt(st.nextToken()); // 문자열 입력 크기
 
 			st = new StringTokenizer(br.readLine());
 			String input = st.nextToken();
 
 			char[] commandArr = input.toCharArray();
-			int[] loofChk = new int[commandArr.length];
-
+			int[] loofChkNum = new int[commandArr.length];
+			Arrays.fill(loofChkNum, -1);
+			int loofPointer = 0;
 			st = new StringTokenizer(br.readLine());
 			input = st.nextToken();
 			char[] messageArr = input.toCharArray();
@@ -33,6 +34,7 @@ public class Solution {
 			int[] memory = new int[M];
 			int memoryPointer = 0;
 			int commandPointer = 0;
+			int oppositionpointer = 0;
 			int charPointer = 0;
 			int count = 0;
 			boolean isLoof = false;
@@ -41,8 +43,8 @@ public class Solution {
 				count++;
 
 				// 무한루프
-				if (count >= 50000000) {
-					isLoof = true;
+				if (count > 50000000) {
+//					isLoof = true;
 					break;
 				}
 
@@ -85,40 +87,89 @@ public class Solution {
 
 				case '[':
 					if (memory[memoryPointer] == 0) {
-						loofChk[memoryPointer]++;
-						int outCount = 1;
-						for (int idx = commandPointer; idx < commandArr.length; idx++) {
-							if (commandArr[idx] == '[') {
-								outCount++;
-							} else if (commandArr[idx] == ']') {
-								outCount--;
-								if (outCount == 0) {
-									loofChk[idx]++;
-									break;
+						// 반복된 루프
+						if (loofChkNum[commandPointer] == memory[memoryPointer]) {
+
+							isLoof = true;
+							loofPointer = commandPointer;
+
+							int outCount = 0;
+							for (int idx = commandPointer; idx < commandArr.length; idx++) {
+								if (commandArr[idx] == '[') {
+									outCount++;
+								} else if (commandArr[idx] == ']') {
+									outCount--;
+									if (outCount == 0) {
+										oppositionpointer = idx;
+										break;
+									}
+								}
+							}
+
+							// 루프는 아님
+						} else {
+							loofChkNum[commandPointer] = memory[memoryPointer];
+
+							int outCount = 0;
+							for (int idx = commandPointer; idx < commandArr.length; idx++) {
+								if (commandArr[idx] == '[') {
+									outCount++;
+								} else if (commandArr[idx] == ']') {
+									outCount--;
+									if (outCount == 0) {
+										commandPointer = idx;
+										break;
+									}
 								}
 							}
 						}
+
 					} else
 						commandPointer++;
 
 					break;
 				case ']':
 					if (memory[memoryPointer] != 0) {
-						loofChk[memoryPointer]++;
-						int outCount = 1;
-						for (int idx = commandPointer; idx >= 0; idx--) {
-							if (commandArr[idx] == ']') {
-								outCount++;
-							} else if (commandArr[idx] == '[') {
-								outCount--;
-								if (outCount == 0) {
-									loofChk[idx]++;
-									break;
+						// 반복된 루프
+						if (loofChkNum[commandPointer] == memory[memoryPointer]) {
+
+							isLoof = true;
+							loofPointer = commandPointer;
+
+							int outCount = 0;
+							for (int idx = commandPointer; idx >= 0; idx--) {
+								if (commandArr[idx] == ']') {
+									outCount++;
+								} else if (commandArr[idx] == '[') {
+									outCount--;
+									if (outCount == 0) {
+										oppositionpointer = idx;
+										break;
+									}
+								}
+							}
+
+							// 루프는 아님
+						} else {
+							loofChkNum[commandPointer] = memory[memoryPointer];
+
+							int outCount = 0;
+							for (int idx = commandPointer; idx >= 0; idx--) {
+								if (commandArr[idx] == ']') {
+									outCount++;
+								} else if (commandArr[idx] == '[') {
+									outCount--;
+									if (outCount == 0) {
+										commandPointer = idx;
+										break;
+									}
 								}
 							}
 						}
+
 					} else
 						commandPointer++;
+
 					break;
 				case '.':
 					commandPointer++;
@@ -140,27 +191,12 @@ public class Solution {
 			}
 
 			if (isLoof) {
-				System.out.println(Arrays.toString(loofChk));
-				int max = 0;
-				int maxIdx = 0;
-				int secondMax = 0;
-				int secondMaxIdx = 0;
-				for (int i = 0; i < loofChk.length; i++) {
-					if (max < loofChk[i]) {
-						max = loofChk[i];
-						maxIdx = i;
-					}
-				}
-				for (int i = 0; i < loofChk.length; i++) {
-					if (secondMax < loofChk[i] && i != maxIdx) {
-						secondMax = loofChk[i];
-						secondMaxIdx = i;
-					}
-				}
+				System.out.println("Loops " + Math.min(loofPointer, oppositionpointer) + " "
+						+ Math.max(loofPointer, oppositionpointer));
 
-				System.out.println("Loops " + secondMaxIdx + " " + maxIdx);
-			} else
+			} else {
 				System.out.println("Terminates");
+			}
 		}
 	}
 
@@ -168,4 +204,9 @@ public class Solution {
 			+ "100 74 4\r\n" + "+++++[->++<]>[-<+++++++>]<[->+>+>+>+<<<<]>+++>--->++++++++++>---<<<.>.>.>.\r\n"
 			+ "xxyz\r\n" + "9999 52 14\r\n" + "+++++[>+++++++++<-],+[-[>--.++>+<<-]>+.->[<.>-]<<,+]\r\n"
 			+ "this_is_a_test";
+
+	static String TC1 = "1\r\n" + "1000 5 1\r\n" + "+[+-]\r\n" + "a";
+
+	static String TC2 = "1\r\n" + "1000 10 1\r\n" + "+[,[+-]-].\r\n" + "a";
+	static String TC3 = "1\r\n" + "10 9 3\r\n" + "+[-[><]-]\r\n" + "qwe";
 }
