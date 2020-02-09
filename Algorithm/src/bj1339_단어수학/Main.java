389bb9bf7f177dd951a53516ad39d846e599e40c
NobleTuna <lyy7661@naver.com>
2020-02-09 22:21:57 +0900

    bj1339 단어수학 푸는중

---
+++ Algorithm/src/bj1339_단어수학/Main.java
@@ -0,0 +1,32 @@
+package bj1339_단어수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+//		char A = 'A';
+//		int k = A+0;
+//		System.out.println(k); A = 65, 26개
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		int[] alpha = new int[26];
+		String[] inputs = new String[N];
+
+		
+		for (int n = 0; n < N; n++) {
+			inputs[n] = br.readLine();
+		}
+		
+		
+		
+		for(int i=0; i<N; i++) {
+			
+		}
+	}
+
+}
