0ebe1e376ad92a6ae3f378dc89814aedfa3d6ee1
NobleTuna <lyy7661@naver.com>
2020-02-16 22:32:38 +0900

    bj6549 인덱스를 저장하는걸로 변경, 탐색 구현중

--- Algorithm/src/bj6549_histogram/Main.java
+++ Algorithm/src/bj6549_histogram/Main.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
+	static final int INF = Integer.MAX_VALUE;
 	static int N;
 
 	static long answer;
@@ -32,7 +32,7 @@ public class Main {
 		while (true) {
 			for (int i = 0; i < tree.length; i++) {
 
-				tree[i] = Integer.MAX_VALUE;
+				tree[i] = INF;
 
 			}
 
@@ -76,8 +76,8 @@ public class Main {
 //			
 
 			for (int i = leafNodeS - 1; i > 0; i--) {
-
-				tree[i] = Math.min(tree[i * 2], tree[i * 2 + 1]);
+//				System.out.println(i);
+				tree[i] = (tree[i * 2] < tree[i * 2 + 1]) ? i * 2 : i * 2 + 1;
 
 				from[i] = from[i * 2];
 
@@ -87,30 +87,62 @@ public class Main {
 					to[i] = to[i * 2 + 1];
 
 			}
-			/**  // 탐색부분 잘못됨
-			 * for (int i = 1; i < End; i++) { if (tree[i] != Integer.MAX_VALUE) answer =
-			 * Math.max((to[i] - from[i] + 1) * tree[i], answer);
+			/**
+			 * // 탐색부분 잘못됨 for (int i = 1; i < End; i++) { if (tree[i] != Integer.MAX_VALUE)
+			 * answer = Math.max((to[i] - from[i] + 1) * tree[i], answer);
 			 * 
 			 * }
 			 **/
 
-//			for (int i = 0; i < 18; i++) {
-//				System.out.print(tree[i] + " ");
-//			}
-//			System.out.println();
-//			for (int i = 0; i < 18; i++) {
-//				System.out.print(from[i] + " ");
-//			}
+			System.out.print("TREE ");
+			for (int i = 0; i < 18; i++) {
+				System.out.print(tree[i] + " ");
+			}
+			System.out.println();
+			for (int i = 0; i < 18; i++) {
+				System.out.print(from[i] + " ");
+			}
 //			System.out.println();
 //			for (int i = 0; i < 18; i++) {
 //				System.out.print(to[i] + " ");
 //			}
 			System.out.println();
 //			System.out.println(Arrays.toString(to));
-
-			System.out.println(answer);
+			System.out.println("calc : " + calc(1, 4, 1));
+//			System.out.println(answer);
 
 		}
+	}
+
+	/**
+	 * 
+	 * @param start 찾을 범위 시작점
+	 * @param end 찾을 범위 끝점
+	 * @param idx 현재 인덱스
+	 * @return 최소값 인덱스 리턴 (tree[] 값)
+	 */
+	public static int calc(int start, int end, int idx) {
+
+		if (idx >= 400001 || idx < 0)
+			return 0;
+
+		if (start <= from[idx] && to[idx] <= end) {
+//			System.out.println("return" + tree[idx]);
+			return tree[idx];
+		}
+
+		int leftValue = 0;
+		int rightValue = 0;
+		if (start > from[idx])
+			leftValue = calc(start, end, idx * 2 + 1);
+
+		if (end < to[idx])
+			rightValue = calc(start, end, idx * 2);
+
+//		System.out.println("left: " + leftValue);
+//		System.out.println("right: " + rightValue);
+		return Math.min(tree[leftValue], tree[rightValue]);
+
 	}
 
 	static String TC1 = "1\r\n" + "7\r\n" + "2 1 4 5 1 3 3";
